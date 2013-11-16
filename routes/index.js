deedc4be55ac85456e4a4b87368eb7986c8fced1
sergey <sergey@debianServer>
2013-11-16 15:31:43 +0200

    Only mongoose is used for db

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,10 @@ var db = require('./../db');
 
 function getTrips(type, filter, callback) {
 	// type : boolean - if it's true we're looking for orders otherwise for offers
-	filter[type ? "passenger" :"driver"] = {$exists : true};
+	//filter[type ? "passenger" :"driver"] = {$exists : true};
 	console.log(filter);
-	db.get('trips', filter, callback);
+	db.getTrips(type, filter, callback)
+	//db.get('trips', filter, callback);
 }
 
 exports.index = function(req, res, type){
@@ -17,3 +18,14 @@ exports.index = function(req, res, type){
 		res.render('index', {result : data, RequestedUserType : (type ? "passenger" : "driver")});
 	});
 };
+exports.login = function(req, res){
+	db.saveUser({firstName : "Mickey",lastName : "Mouse"}, function(err, data){
+		res.render('customer', data);
+	});
+}
+exports.saveTrip = function(req, res) {
+	db.createTrip(1, {x: 34, y: 85}, {x: 50, y: 154}, 100, function(err, data){
+		console.log(data);
+		res.render('tripCreatedMessage', data);
+	})
+}
