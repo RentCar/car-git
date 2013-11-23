6887d7a25fbed48d575d199de6f9d28ae7f5aef1
sergey <sergey@debianServer>
2013-11-23 13:10:53 +0200

    refactored pgmodel added pQuery function for parameter pg queries, finished registration and authification from facebook

--- pgModel.js
+++ pgModel.js
@@ -2,25 +2,41 @@ var pg = require('pg');
 var dbConnectionConfig = { host: 'localhost', user: 'car', password: 'qwerty', database: 'destination' };
 var conString = "postgres://car:qwerty@localhost/destination";
 var client = new pg.Client(conString);
+
+client.pQuery = function(query, params, callback){
+	if(query && params && callback) {
+		this.query(query.replace(/\$(\d+)/g, function(m1, m2){
+			return (""+params[m2]).replace(/(\"|\')/g, "\\$1") || m1;
+		}), callback);
+	}
+	else {
+		callback("some of query, params, callback input parameter isn't passed");
+	}
+}
+
 client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  console.log((new Date()).getTime());
-  client.query("", function(err, result) {
-	console.log(result);
-    if(err) {
-      return console.error('error running query', err);
-    }
-	 console.log((new Date()).getTime());
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-   // client.end();
-  });
+	if(err) {
+	return console.error('could not connect to postgres', err);
+	}
 });
+
 
 exports.connect = client.connect;
 exports.addOrGetUser = function(socialNetworkId, profile, callback) {
-	client.query("select id, first_name, last_name, karma from dest.users where id = (select user_id from dest.user_social where social_id='"+profile.id+"'  and social_type_id = '"+socialNetworkId+"')", callback);
-	//client.query("insert into dest.users (first_name, last_name) values ('"+profile._json.first_name+"', '"+profile._json.last_name+"')", callback);
-	
-}
+	client.pQuery("select id, first_name, last_name, karma from dest.users where id = \
+	(select user_id from dest.user_social where social_id='$0'  and social_type_id = '$1')", [profile.id, socialNetworkId],
+	function(err, result){
+		if(result && !result.rowCount > 0) {
+			client.pQuery("with row as (insert into dest.users (first_name, last_name) values('$0', '$1') RETURNING id)\
+			insert into dest.user_social (social_id, social_type_id, user_id)\
+			select '$2' as social_id, '$3' as social_type_id, u.id as user_id from row as u",
+			[profile._json.first_name, profile._json.last_name, profile.id, socialNetworkId],
+			function(err, result){
+				callback(err, profile);
+			});
+		}
+		else{
+			callback(err, result);
+		}
+	});
+};
