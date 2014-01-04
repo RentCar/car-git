98f154c839bc95bb5ca7e9a9e7d4b2aa5f6a595e
sergey <sergey@debianServer>
2014-01-04 18:31:30 +0200

    trip saving emit event with new trip

--- socket.js
+++ socket.js
@@ -14,25 +14,26 @@ exports.init = function(server, sessionStore, cookieParser) {
 			var points = [];
 			for(var key in data.points) {
 				points.push({
-					lat: data.points[key].geopoints.lat, 
-					lng: data.points[key].geopoints.lng, 
+					latlng : {
+						lat: data.points[key].geopoints.lat, 
+						lng: data.points[key].geopoints.lng
+					}, 
 					address: data.points[key].address
 				});
-			}
+			}			
 			db.createOrder(session.passport.user, points, data.price, (new Date()).getTime(), 
 				function(err, trip){
 					var newtrip = {
 						"_id" : trip._id,
 						route : {points:points},
 						users : [null, session.passport.user],
-						startPrice : data.price
-					};
-					console.log("aaaaaaaaaaaaaaaaaaaaaaaaaa");
-					console.log(newtrip);
+                        startPrice : data.price
+                    };
 					if(err) {
 						socket.emit("tripSavingError", {reason: "An error has been occured while trip saving", err: err});
 					}
 					else {
+						console.log(trip);
 						socket.broadcast.emit("onNewOrder", newtrip);
 						socket.emit("tripSaved", newtrip);
 					}
