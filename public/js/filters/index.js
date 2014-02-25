adcfee4b78c466b960c179393f7fc32e4e215a61
artem <murdoc.ak@gmail.com>
2014-02-25 20:54:08 +0200

    refactoring pt.3  js crash fix

--- public/js/filters/index.js
+++ public/js/filters/index.js
@@ -2,12 +2,6 @@
  * Created by Artem on 17.11.13.
  */
 
-//define([
-//
-//], function () {
-//    console.log("Defined all filters")
-//});
-
 define(['angular'], function (ng) {
     'use strict';
     console.log("Returning module 'app.filters", ng);
--- public/js/services/socketsFactory.js
+++ public/js/services/socketsFactory.js
@@ -2,26 +2,12 @@
  * Factory for socket.io lib
  * commented us example
  */
-
 // loads to init file (require)
 define(['./init', 'socket.io'], function (services, io) {
     'use strict';
-    console.log("Init Socket factory")
-    // added 'socket' factory to services array
-    // init factory (angular)
     services.factory('socket', [function () {
-
-        console.log("socket factory: ", io)
-
         //connection to default
         var socket = io.connect(location.origin);
-
-        // test query
-        socket.on('newUser', function (data) {
-            console.log(data);
-            socket.emit('data', { my: 'data' });
-        });
-
         // return sockets object
         return socket
     }]);
